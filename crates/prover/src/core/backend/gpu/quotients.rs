6ef19bc3dee0778751251276eb93285ee0c833a1
danielntmd <daniel.kim@nethermind.io>
2024-07-07 23:24:33 -0700

    initial

--- crates/prover/src/core/backend/gpu/quotients.rs
+++ crates/prover/src/core/backend/gpu/quotients.rs
@@ -1,17 +1,19 @@
 use super::GpuBackend;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
+use crate::core::fields::secure_column::SecureColumn;
 use crate::core::pcs::quotients::{ColumnSampleBatch, QuotientOps};
 use crate::core::poly::circle::{CircleDomain, CircleEvaluation, SecureEvaluation};
 use crate::core::poly::BitReversedOrder;
 
 impl QuotientOps for GpuBackend {
     fn accumulate_quotients(
-        _domain: CircleDomain,
-        _columns: &[&CircleEvaluation<Self, BaseField, BitReversedOrder>],
-        _random_coeff: SecureField,
-        _sample_batches: &[ColumnSampleBatch],
+        domain: CircleDomain,
+        columns: &[&CircleEvaluation<Self, BaseField, BitReversedOrder>],
+        random_coeff: SecureField,
+        sample_batches: &[ColumnSampleBatch],
     ) -> SecureEvaluation<Self> {
-        todo!()
+        let mut values = SecureColumn::<Self>::zeros(domain.size());
+        
     }
 }
