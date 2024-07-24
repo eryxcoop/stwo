688ab87d0f0951a26b6c04a4802fb64dbbf09390
carlogf <cgferrari@gmail.com>
2024-07-24 16:55:59 -0300

    wip rename merklehasher generic

--- crates/prover/src/core/pcs/verifier.rs
+++ crates/prover/src/core/pcs/verifier.rs
@@ -47,7 +47,7 @@ impl<MH: MerkleHasher> CommitmentSchemeVerifier<MH> {
             .iter()
             .map(|&log_size| log_size + LOG_BLOWUP_FACTOR)
             .collect();
-        let verifier = MerkleVerifier::<MH>::new(commitment, extended_log_sizes);
+        let verifier = MerkleVerifier::new(commitment, extended_log_sizes);
         self.trees.push(verifier);
     }
 
@@ -80,7 +80,7 @@ impl<MH: MerkleHasher> CommitmentSchemeVerifier<MH> {
         // FRI commitment phase on OODS quotients.
         let fri_config = FriConfig::new(LOG_LAST_LAYER_DEGREE_BOUND, LOG_BLOWUP_FACTOR, N_QUERIES);
         let mut fri_verifier =
-            FriVerifier::<MH>::commit(channel, fri_config, proof.fri_proof, bounds)?;
+            FriVerifier::commit(channel, fri_config, proof.fri_proof, bounds)?;
 
         // Verify proof of work.
         ProofOfWork::new(PROOF_OF_WORK_BITS).verify(channel, &proof.proof_of_work)?;
